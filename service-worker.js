0ad3f3a4fb5e93c3123075fb02ca7aa413000a2d
beemerspb <89839312+beemerspb@users.noreply.github.com>
2025-04-20 19:51:10 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("app-cache-v1").then((cache) => {
+    caches.open("app-cache").then((cache) => {
       return cache.addAll([
         "./",
         "./index.html",
@@ -9,21 +9,6 @@ self.addEventListener("install", (event) => {
         "./icon-192.png",
         "./icon-512.png"
       ]);
-    })
-  );
-});
-
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = ['app-cache-v1']; // Обновите версию кэша
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
     })
   );
 });
